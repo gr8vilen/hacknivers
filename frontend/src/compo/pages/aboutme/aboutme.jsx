3b9ada8ad9a5f9ff3592966c81f111e44c3ede2c
gr8vilen <admin@gr8vilen.tk>
2022-04-12 18:29:51 +0530

    added cirt

--- frontend/src/compo/pages/aboutme/aboutme.jsx
+++ frontend/src/compo/pages/aboutme/aboutme.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './header/header'
 import Navbar from '../../nav/navbar'
 import Exp from './exp/exp'
+import Cirt from './cirt/cirt'
 function Aboutme() {
   return (
     <div>
       <Header/>
       <Exp/>
+      <cirt/>
       <Navbar/>
     </div>
   )
---
+++ frontend/src/compo/pages/aboutme/cirt/cirt.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './cirt.sass'
+
+function Cirt() {
+  return (
+    <div>Cirt</div>
+  )
+}
+
+export default Cirt
